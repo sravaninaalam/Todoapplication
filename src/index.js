2ff5318f6941030dd16b5f5ac0a371d6acdfdac0
sravaninaalam <sravaninaalam2000@gmail.com>
2023-08-25 11:05:07 +0530

    modified

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+
 import reportWebVitals from './reportWebVitals';
-import bootstrap from 'bootstrap/dist/css/bootstrap.min.css'
+import Todo from './Todo';
+// import bootstrap from 'bootstrap/dist/css/bootstrap.min.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+   <Todo/>
   </React.StrictMode>
 );
 
